4c09b9b74cb9618d9fa9f0551e0768e237d8a231
Abubakir Myrzaly <best.abuka@gmail.com>
2017-08-17 10:59:44 -0400

    Created ScoreBoard class

---
+++ COMP123-S2017-FinalExam-StudentID/ScoreBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Abubakir Myrzaly
+ * Date: 8/17/2017
+ * StudentID: 300931945
+ * Description: ScoreBoard class
+ * Version: 0.1 Just create class
+ */
+
+namespace COMP123_S2017_FinalExam_StudentID
+{
+    class ScoreBoard
+    {
+    }
+}
