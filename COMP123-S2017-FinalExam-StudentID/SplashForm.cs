8a91859d3afd1643782c269c81a2fbf801e324bc
Abubakir Myrzaly <best.abuka@gmail.com>
2017-08-17 10:57:42 -0400

    Finish my splash form

--- COMP123-S2017-FinalExam-StudentID/SplashForm.cs
+++ COMP123-S2017-FinalExam-StudentID/SplashForm.cs
@@ -8,6 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name: Abubakir Myrzaly
+ * Date: 8/17/2017
+ * StudentID: 300931945
+ * Description: Splash Form
+ * Version: 0.1 Created Splash form
+ */
+
 namespace COMP123_S2017_FinalExam_StudentID
 {
     public partial class SplashForm : Form
@@ -15,6 +23,15 @@ namespace COMP123_S2017_FinalExam_StudentID
         public SplashForm()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            PickHighestCardForm pickHighestCardForm = new PickHighestCardForm();
+            pickHighestCardForm.Show();
+            this.Hide();
+
+            SplashFormTimer.Enabled = false;
         }
     }
 }
