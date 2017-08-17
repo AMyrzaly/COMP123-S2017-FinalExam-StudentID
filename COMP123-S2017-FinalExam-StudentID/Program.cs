bee8a05330e56e2b62cf0e191873d56d271bc661
Abubakir Myrzaly <best.abuka@gmail.com>
2017-08-17 10:49:34 -0400

    Added comments to Drive class and start creating splash form

--- COMP123-S2017-FinalExam-StudentID/Program.cs
+++ COMP123-S2017-FinalExam-StudentID/Program.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 /*
- * Name:
- * Date:
- * StudentID:
- * Description:
- * Version:
+ * Name: Abubakir Myrzaly
+ * Date: 8/17/2017
+ * StudentID: 300931945
+ * Description: Drive Class
+ * Version: 0.1 Just creat form
  */
 
 namespace COMP123_S2017_FinalExam_StudentID
